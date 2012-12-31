4f00bd195f0bb6001e57eaa3d7a4976df64c0678
thoughtadvances <thoughtadvances@icloud.com>
2012-12-31 16:00:38 +0545

    Set initial image zoom to show as much as possible with no whitespace

--- FlickrExplorer/PhotoViewController.m
+++ FlickrExplorer/PhotoViewController.m
@@ -24,15 +24,34 @@
     self.scrollView.delegate = self; // I tell the scrollView what to do
 }
 
-- (void)viewDidLayoutSubviews {
-    // TOOD: Set the photo size information so that as much as possible is shown
+- (void)viewWillAppear:(BOOL)animated {
+    // TODO: Set the photo size information so that as much as possible is shown
     self.imageView.image = self.image; // set the image on screen
     
     // set bounds
     self.scrollView.contentSize = self.imageView.image.size;
     self.imageView.frame = CGRectMake(0, 0, self.imageView.image.size.width,
                                       self.imageView.image.size.height);
-
+    
+    // Calculate size differences between scrollView and the image
+    double scale = 0.0;
+    double widthDifference = fabs(self.scrollView.bounds.size.width -
+                                  self.imageView.image.size.width);
+    double heightDifference = fabs(self.scrollView.bounds.size.height -
+                                   self.imageView.image.size.height);
+    
+    if (widthDifference > heightDifference) { // stretch height to fit
+        scale = self.scrollView.bounds.size.height /
+        self.imageView.image.size.height;
+    } else { // stretch width to fit
+        scale = self.scrollView.bounds.size.width /
+        self.imageView.image.size.width;
+    }
+    
+    // set initial scale to show as much of image as possible without any
+    //  whitespace
+    self.scrollView.zoomScale = scale;
+    
 }
 
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
