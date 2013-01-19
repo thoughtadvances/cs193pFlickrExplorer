632dba100dfc9ba71c6489e94e921172643c2092
thoughtadvances <thoughtadvances@icloud.com>
2013-01-19 14:23:37 +0545

    Show all annotations on screen, Implement segue preparations

--- FlickrExplorer/MapViewController.m
+++ FlickrExplorer/MapViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "MapViewController.h"
+#import "FlickrPhotoViewController.h"
+#import "FlickrPhotoAnnotation.h"
+#import "FlickrPhotoSelectorTableViewController.h"
 
 @interface MapViewController () <MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet UISegmentedControl *mapTypeSelector;
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @end
 
@@ -30,15 +34,6 @@
     if (self.annotations) [self.mapView addAnnotations:self.annotations];
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (MKAnnotationView *)mapView:(MKMapView *)mapView
             viewForAnnotation:(id<MKAnnotation>)annotation {
     MKAnnotationView *view = [mapView
@@ -50,33 +45,98 @@
         view.canShowCallout = YES;
         view.leftCalloutAccessoryView = [[UIImageView alloc] initWithFrame:
                                          CGRectMake(0, 0, 30, 30)];
+        view.rightCalloutAccessoryView = [UIButton buttonWithType:
+                                          UIButtonTypeDetailDisclosure];
     }
     view.annotation = annotation;
     [(UIImageView *)view.leftCalloutAccessoryView setImage:nil];
     return view;
 }
 
-- (void)mapView:(MKMapView *)mapView didDeselectAnnotationView:
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {
+    [self performSegueWithIdentifier:@"showMapPhoto" sender:view.annotation];
+}
+
+- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:
 (MKAnnotationView *)view {
     UIImage *image = [self.delegate mapViewController:self
                                    imageForAnnotation:view.annotation];
     [(UIImageView *)view.leftCalloutAccessoryView setImage:image];
 }
 
+- (void)setMapType {
+    NSInteger choice = self.mapTypeSelector.selectedSegmentIndex;
+    if (choice == 0) self.mapView.mapType = MKMapTypeStandard;
+    else if (choice == 1) self.mapView.mapType = MKMapTypeSatellite;
+    else self.mapView.mapType = MKMapTypeHybrid;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.mapView.delegate = self;
+    self.mapTypeSelector.selectedSegmentIndex = 0;
+    [self.mapTypeSelector addTarget:self.mapView
+                             action:@selector(setMapType:)
+                   forControlEvents:UIControlEventValueChanged];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    // Show all annotations on screen
+    double maxLatitude = 0;
+    double minLatitude = 0;
+    double maxLongitude = 0;
+    double minLongitude = 0;
+    for (id<MKAnnotation> annotation in self.annotations) { // get bounds
+        if (annotation.coordinate.latitude) {
+            if (annotation.coordinate.latitude > maxLatitude)
+                maxLatitude = annotation.coordinate.latitude;
+            else if (annotation.coordinate.latitude < minLatitude)
+                minLatitude = annotation.coordinate.latitude;
+            if (annotation.coordinate.longitude > maxLongitude)
+                maxLongitude = annotation.coordinate.longitude;
+            else if (annotation.coordinate.longitude < minLongitude)
+                minLongitude = annotation.coordinate.longitude;
+        }
+    }
+    // width and height
+    CLLocationCoordinate2D bottomLeftPoint = {minLatitude, minLongitude};
+    CLLocationCoordinate2D topRightPoint = {maxLatitude, maxLongitude};
+    double width = MKMapPointForCoordinate(topRightPoint).x -
+    MKMapPointForCoordinate(bottomLeftPoint).x;
+    double height = MKMapPointForCoordinate(topRightPoint).y -
+    MKMapPointForCoordinate(bottomLeftPoint).y;
+    // center
+    double latitudeCenter = (maxLatitude - minLatitude ) / 2;
+    double longitudeCenter = (maxLongitude - minLongitude) / 2;
+    CLLocationCoordinate2D center = {latitudeCenter, longitudeCenter};
+    // set it
+    MKMapRect finalRegion = {MKMapPointForCoordinate(center),
+        MKMapSizeMake(width, height)};
+    [self.mapView setVisibleMapRect:finalRegion
+                        edgePadding:UIEdgeInsetsMake(10.0, 10.0, 10.0, 10.0) animated:NO];
 }
 
 - (NSUInteger)supportedInterfaceOrientations {
     return UIInterfaceOrientationMaskAll;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"showMapPhoto"]) {
+        FlickrPhotoAnnotation *flickrAnnotation =
+        (FlickrPhotoAnnotation *)sender;
+        [segue.destinationViewController setPhoto:flickrAnnotation.photo];
+    }
+    else if ([segue.identifier isEqualToString:@"showTablePhoto"]) {
+        NSLog(@"I got to MapViewController's prepareForSegue method");
+        NSLog(@"Class of sender is %@", [sender class]);
+        if ([sender isKindOfClass:[FlickrPhotoSelectorTableViewController
+                                   class]]) {
+            NSLog(@"sender is of the right type");
+        }
+        [segue.destinationViewController setPhoto:[sender selectedPhoto]];
+        NSLog(@"Successfuly set the destination's photo");
+    }
 }
 
 @end
