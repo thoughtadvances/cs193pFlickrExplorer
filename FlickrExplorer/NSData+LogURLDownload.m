5e91570afb71a608a278fe9c1ec7a644cc41469e
thoughtadvances <thoughtadvances@icloud.com>
2013-01-28 18:01:13 +0530

    Remove inferior swizzling implementation

--- FlickrExplorer/NSData+LogURLDownload.m
+++ FlickrExplorer/NSData+LogURLDownload.m
@@ -13,30 +13,6 @@
 
 @implementation NSData (LogURLDownload)
 
-//void MethodSwizzle(Class aClass, SEL orig_sel, SEL alt_sel) {
-//    Method orig_method = nil, alt_method = nil;
-//    // First, look for the methods
-//    orig_method = class_getInstanceMethod(aClass, orig_sel);
-//    alt_method = class_getInstanceMethod(aClass, alt_sel);
-//    // If both are found, swizzle them
-//    if ((orig_method != nil) && (alt_method != nil))
-//    {
-//        char temp1;
-//        IMP temp2;
-//        temp1 = orig_method->method_types;
-//        orig_method->method_types = alt_method->method_types;
-//        alt_method->method_types = temp1;
-//        temp2 = orig_method->method_imp;
-//        orig_method->method_imp = alt_method->method_imp;
-//        alt_method->method_imp = temp2;
-//    }
-//}
-//
-//// TODO: Are the two following methods equivalent?
-//+ (void)load {
-//    MethodSwizzle();
-//}
-
 // TODO: Create a method that takes two selectors and swizzles them
 + (void)load {
     Method dataWithContentsOfURL =
