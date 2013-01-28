59f53c992e5f3336c31a23ff8f9729b3f07db098
thoughtadvances <thoughtadvances@icloud.com>
2013-01-28 17:57:55 +0530

    Add support class of convenience utilities for dealing with UIViewControllers

--- FlickrExplorer/ViewControllerSupport.h
+++ FlickrExplorer/ViewControllerSupport.h
@@ -9,5 +9,6 @@
 #import <Foundation/Foundation.h>
 
 @interface ViewControllerSupport : NSObject
-
++ (UIViewController*)getNonNavigationControllerFor:
+(UIViewController *)controller;
 @end
--- FlickrExplorer/ViewControllerSupport.m
+++ FlickrExplorer/ViewControllerSupport.m
@@ -9,5 +9,41 @@
 #import "ViewControllerSupport.h"
 
 @implementation ViewControllerSupport
+// TODO: Make this a category method for the UIViewController rather than a new
+//  class
+// TODO: Is there any way to do this without casting?  Why is all this casting
+//  necessary?
++ (UIViewController*)getNonNavigationControllerFor:
+(UIViewController *)controller {
+    if ([controller isKindOfClass:[UINavigationController class]]) {
+        controller = [(UINavigationController*)controller topViewController];
+    }
+    else if ([controller isKindOfClass:[UISplitViewController class]]) {
+        controller = [[(UISplitViewController*)controller viewControllers]
+                      lastObject];
+    }
+    else if ([controller isKindOfClass:[UITabBarController class]]) {
+        controller = [(UITabBarController*)controller selectedViewController];
+    }
+    else if ([controller isKindOfClass:[UINavigationController class]]) {
+        controller = [(UINavigationController*)controller topViewController];
+    }
+    if ([controller isKindOfClass:[UISplitViewController class]] ||
+        [controller isKindOfClass:[UITabBarController class]] ||
+        [controller isKindOfClass:[UINavigationController class]]) {
+        return [ViewControllerSupport getNonNavigationControllerFor:
+                controller];
+    }
+    else return controller;
+}
+
+// Move this to a more general library to work on all obejcts, not just
+//  UIViewControllers
++ (UIViewController*)viewControllerOfType:(Class)class inArray:(NSArray*)array {
+    for (id object in array) {
+        if ([object isKindOfClass:class]) return object;
+    }
+    return nil;
+}
 
 @end
