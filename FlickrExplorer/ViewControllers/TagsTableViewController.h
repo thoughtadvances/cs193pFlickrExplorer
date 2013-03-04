137bac9bb1ea16512a70fe8b1aea9e60a873ad75
thoughtadvances <thoughtadvances@icloud.com>
2013-03-04 22:04:52 +0545

    Add Assignment 6 UIViewController subclasses

---
+++ FlickrExplorer/ViewControllers/TagsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TagsTableViewController.h
+//  FlickrExplorer
+//
+//  Created by admin on 1/M/13.
+//  Copyright (c) 2013 ThoughtAdvances. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TagsTableViewController : UITableViewController
+
+@end
---
+++ FlickrExplorer/ViewControllers/VacationTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  VacationTableViewController.h
+//  FlickrExplorer
+//
+//  Created by admin on 1/M/13.
+//  Copyright (c) 2013 ThoughtAdvances. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VacationTableViewController : UITableViewController
+
+@end
